6b1c20e987d3c51f19298d0016480ec2cda2829f
krisnadiputra <krisnadiputra@yahoo.com.sg>
2020-01-12 17:42:26 +0900

    defined endpoints for api interface

--- src/main/scala/com/paidy/user/UserServlet.scala
+++ src/main/scala/com/paidy/user/UserServlet.scala
@@ -4,6 +4,42 @@ import org.scalatra._
 
 class UserServlet extends ScalatraServlet {
 
+  post("/users/:id/reset-password") {
+
+  }
+
+  post("/users/:id/unblock") {
+
+  }
+
+  post("/users/:id/block") {
+
+  }
+
+  get("/users/:id") {
+    
+  }
+
+  put("/users/:id") {
+    
+  }
+
+  delete("/users/:id") {
+    
+  }
+
+  get("/users/generate") {
+    
+  }
+
+  post("/users/signup") {
+
+  }
+
+  get("/users") {
+
+  }
+
   get("/") {
     views.html.hello()
   }
